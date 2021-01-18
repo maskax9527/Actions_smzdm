968d7372cf3d11c891d2ad05a2ca6780231adb51
maskax9527 <maskax@163.com>
2021-01-18 13:54:40 +0800

    Update smzdm.js

--- function/smzdm.js
+++ function/smzdm.js
@@ -69,11 +69,11 @@ async function start() {
             console.log("什么值得买签到-" + content)
         }else{
             await notify.sendNotify("什么值得买签到-" + new Date().toLocaleDateString(), content);
-            console.log("什么值得买签到-" + content)
+            console.log("什么值得买签到-无Cookie-" + content)
         }
     }else{
         // await notify.sendNotify("什么值得买签到-" + new Date().toLocaleDateString(), content);
-        console.log("什么值得买签到-不推送消息" + content)
+        console.log("什么值得买签到-不推送消息-" + content)
     }
 
     //运行完成后，删除下载的文件
